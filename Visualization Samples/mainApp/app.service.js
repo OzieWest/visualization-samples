b7e732e3819c3783f2b19e5e03076faab5f80cb4
SoraMusoka <soramusoka@gmail.com>
2014-02-17 23:40:02 +0400

    добавлен localforage.js, переделан метод запроса большого списка друзей.

--- Visualization Samples/mainApp/app.service.js
+++ Visualization Samples/mainApp/app.service.js
@@ -3,22 +3,22 @@
 var mainService = angular.module('vis.service', []);
 var ROOT = function (method) { return 'https://api.vk.com/method/' + method + '?access_token=' + token; };
 
-function defCall(Q, H, id) {
-	var deferred = Q.defer();
-	var path = ROOT('friends.get') + '&user_id=' + id + "&fields=last_name";
-	var p = H.jsonp(path + '&callback=JSON_CALLBACK');
-	p.success(function (data) {
-		deferred.resolve(data.response);
-	});
-	p.error(function (err) {
-		console.error('friendsGet error', err);
-		deferred.reject();
-	});
-	return deferred.promise;
-}
-
 mainService.factory('vkService', function ($http, $q) {
 	return {
+		usersGet: function (_uids) {
+			var deferred = $q.defer();
+			var path = ROOT('users.get') + '&uids=' + _uids + '&fields=sex,bdate,city,country,lists,screen_name,has_mobile,contacts,education,universities,schools,can_post,can_see_all_posts,can_write_private_message,status,last_seen,relation,counters,nickname=';
+			var p = $http.jsonp(path + '&callback=JSON_CALLBACK');
+			p.success(function (data) {
+				console.info('usersGet', 'return', data.response);
+				deferred.resolve(data.response);
+			});
+			p.error(function (err) {
+				console.error('usersGet error', err);
+				deferred.reject();
+			});
+			return deferred.promise;
+		},
 		wallGet: function (_ownerId, _offset, _count, _filter, _extended) {
 			var deferred = $q.defer();
 			var path = ROOT('wall.get') + '&count=' + _count + '&offset=' + _offset + '&owner_id=' + _ownerId;
@@ -47,36 +47,35 @@ mainService.factory('vkService', function ($http, $q) {
 			});
 			return deferred.promise;
 		},
-		usersGet: function (_uids) {
-			var deferred = $q.defer();
-			var path = ROOT('users.get') + '&uids=' + _uids + '&fields=sex,bdate,city,country,lists,screen_name,has_mobile,contacts,education,universities,schools,can_post,can_see_all_posts,can_write_private_message,status,last_seen,relation,counters,nickname=';
-			var p = $http.jsonp(path + '&callback=JSON_CALLBACK');
-			p.success(function (data) {
-				console.info('usersGet', 'return', data.response);
-				deferred.resolve(data.response);
-			});
-			p.error(function (err) {
-				console.error('usersGet error', err);
-				deferred.reject();
-			});
-			return deferred.promise;
-		}
+		friendAllGet: function () {
+			var test = {
+				a: {},
+				b: {}
+			};
+			
+			//var a = ;
+			//var a = getFriendCallback('1563369');
+
+			setTimeout(getFriendCallback('169483251', test.a), 1000);
+			setTimeout(getFriendCallback('1563369', test.a), 2000);
+
+			return test;
+		},
 	};
 });
 
-mainService.factory('testService', function ($http, $q) {
-	return {
-		friendsGroupGet: function () {
-			var deferred = $q.defer();
-			var p = $http.get('../api/test/get');
-			p.success(function (data) {
-				deferred.resolve(data);
-			});
-			p.error(function (err) {
-				console.error('friendsGroupGet', err);
-				deferred.reject();
-			});
-			return deferred.promise;
-		}
-	};
-});
+
+function getFriendCallback($q, $http, id, prom) {
+	var deferred = $q.defer();
+	var path = ROOT('friends.get') + '&user_id=' + id;
+	var p = $http.jsonp(path + '&callback=JSON_CALLBACK');
+	p.success(function(data) {
+		console.info('friendsGet', id, 'return', data.response);
+		deferred.resolve(data.response);
+	});
+	p.error(function(err) {
+		console.error('friendsGet error', err);
+		deferred.reject();
+	});
+	prom = deferred.promise;
+}
