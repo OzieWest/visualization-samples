3157102340cb341d7b26b741230ecff20b11f2e0
SoraMusoka <soramusoka@gmail.com>
2014-02-21 23:34:42 +0400

    График постов доведен до юзабельного состояния, добавлены стили и дом информация

--- Visualization Samples/jsApp/app.module.js
+++ Visualization Samples/jsApp/app.module.js
@@ -16,10 +16,11 @@ angular.module('vis.directives', [])
 				color: '=',
 				w: '=',
 				h: '=',
-				renderer: '='
+				renderer: '=',
+				post: '='
 			},
 			template: '<div></div>',
-			restrict: 'E',
+			restrict: 'A',
 			link: function postLink(scope, element, attrs) {
 				scope.$watchCollection('[data, renderer]', function (newVal, oldVal) {
 					if (!newVal[0]) {
@@ -35,23 +36,54 @@ angular.module('vis.directives', [])
 						renderer: scope.renderer
 					});
 					
-					var xAxis = new Rickshaw.Graph.Axis.X({ graph: graph});
+					var xAxis = new Rickshaw.Graph.Axis.X({ graph: graph });
 					xAxis.render();
 
 					var yAxis = new Rickshaw.Graph.Axis.Y({ graph: graph });
 					yAxis.render();
 					
 					graph.render();
-					
-					var a = new Rickshaw.Graph.HoverDetail({
-						graph: graph,
-						formatter: function (series, x, y) {
-							var date = '<span class="date">' + new Date(x * 1000).toUTCString() + '</span>';
-							var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + '"></span>';
-							var content = swatch + series.name + ": " + parseInt(y) + '<br>' + date;
-							return content;
+
+					element[0].innerHTML += '<div id="legend"></div>';
+		
+					var legend = document.querySelector('#legend');
+					var Hover = Rickshaw.Class.create(Rickshaw.Graph.HoverDetail, {
+						render: function (args) {
+							legend.innerHTML = '';
+							
+							args.detail.sort(function (a, b) { return a.order - b.order; }).forEach(function (d) {
+								var block = document.createElement('div');
+								
+								var label = document.createElement('span');
+								label.className = 'label';
+								label.innerHTML = d.name + ": " + d.formattedYValue;
+								label.style.backgroundColor = d.series.color;
+
+								block.appendChild(label);
+								legend.appendChild(block);
+
+								var dot = document.createElement('div');
+								dot.className = 'dot';
+								dot.style.top = graph.y(d.value.y0 + d.value.y) + 'px';
+								dot.style.borderColor = d.series.color;
+
+								this.element.appendChild(dot);
+
+								dot.className = 'dot active';
+
+								this.show();
+
+							}, this);
+
+							var u = args.points[0].value.url;
+							var purl = document.createElement('span');
+							purl.className = 'label lable-inverse';
+							purl.innerHTML = '<a href="' + u + '" target="_blank">' + u + '</a>';
+							legend.appendChild(purl);
 						}
 					});
+
+					var hover = new Hover({ graph: graph });
 				});
 			}
 		};
